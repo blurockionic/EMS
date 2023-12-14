07577b1c03a55963eb552dfad8d5c4dd60e380f8
unknown <arun-upadhayay>
2023-12-14 16:24:45 +0530

    in newemployee add some points

--- client/src/page/HR/new/NewEmployee.js
+++ client/src/page/HR/new/NewEmployee.js
@@ -44,12 +44,14 @@ const NewEmployee = () => {
           <h2 className="text-2xl font-semibold mb-6">Employee Information</h2>
 
           <div className="grid grid-cols-2 gap-4">
+
+            {/* employees Id  */}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="employeeId"
               >
-                Employee ID
+                ID
               </label>
               <input
                 type="text"
@@ -61,13 +63,14 @@ const NewEmployee = () => {
                 required
               />
             </div>
+            {/* employee name  */}
 
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="employeeName"
               >
-                Employee Name
+                  Name
               </label>
               <input
                 type="text"
@@ -79,6 +82,7 @@ const NewEmployee = () => {
                 required
               />
             </div>
+            {/* gender */}
 
             <div className="mb-4">
               <label
@@ -97,6 +101,261 @@ const NewEmployee = () => {
                 required
               />
             </div>
+
+
+            {/* EmployeeEmail */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="employeeEmail"
+              >
+               Email
+              </label>
+              <input
+                type="email"
+                id="employeeEmail"
+                name="employeeEmail"
+                value={formData.employeeEmail}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* password */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+               Password
+              </label>
+              <input
+                type="password "
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* employeesPhoneNumber  */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="employeePhoneNumber"
+              >
+               Phone Number
+              </label>
+              <input
+                type="text"
+                id="employeePhoneNumber"
+                name="employeePhoneNumber"
+                value={formData.employeePhoneNumber}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* Date of Birth */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="dateOfBirth"
+              >
+               Date of Birth
+              </label>
+              <input
+                type="date"
+                id="dateOfBirth"
+                name="dateOfBirth"
+                value={formData.dateOfBirth}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* Address */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="address"
+              >
+               Address 
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* Post office */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="postOffice"
+              >
+               Post Office 
+              </label>
+              <input
+                type="text"
+                id="postOffice"
+                name="postOffice"
+                value={formData.postOffice}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* policeStation */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="policeStation"
+              >
+               Post Office 
+              </label>
+              <input
+                type="text"
+                id="policeStation"
+                name="policeStation"
+                value={formData.policeStation}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* city */}
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="city"
+              >
+               City 
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                value={formData.city}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* state */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="state"
+               >
+                State
+              </label>
+              <input
+                type="text"
+                id="state"
+                name="state"
+                value={formData.state}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* pinNumber */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="pinNumber"
+               >
+                Pin Code 
+              </label>
+              <input
+                type="number"
+                id="pinNumber"
+                name="pinNumber"
+                value={formData.pinNumber}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* designation */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="designation"
+               >
+                Designation
+              </label>
+              <input
+                type="text"
+                id="designation"
+                name="designation"
+                value={formData.designation}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* designationType */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="designationType"
+               >
+                Designation Type 
+              </label>
+              <input
+                type="text"
+                id="designationType"
+                name="designationType"
+                value={formData.designationType}
+                onChange={handleChange}
+                className="border p-2 w-full"
+                required
+              />
+            </div>
+
+            {/* department */}
+            <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="department"
+              >
+              Designation Type 
+            </label>
+            <input
+              type="text"
+              id="department"
+              name="department"
+              value={formData.department}
+              onChange={handleChange}
+              className="border p-2 w-full"
+              required
+            />
+            </div>
+
+
+
+
+
+
 
             {/* Add more fields as needed */}
           </div>
