08a97774958e347039852cb1dd250383de55b29a
biruly-cyber <biruly2000@gmail.com>
2023-12-18 18:04:38 +0530

    [UPDATE]: UPDATE NEW EMPLOYEE PAGE

--- client/src/page/HR/new/NewEmployee.js
+++ client/src/page/HR/new/NewEmployee.js
@@ -3,9 +3,7 @@ import React, { useState } from "react";
 import { server } from "../../../App";
 
 const NewEmployee = () => {
-
-
-  const [buttonActive, setButtonActive] = useState(false)
+  const [buttonActive, setButtonActive] = useState(false);
   const [formData, setFormData] = useState({
     employeeId: "",
     employeeName: "",
@@ -84,11 +82,11 @@ const NewEmployee = () => {
         withCredentials: true,
       }
     );
-    console.log(response)
+    console.log(response);
 
-    const {success, message} = response.data
-    if(success){
-        alert(message)
+    const { success, message } = response.data;
+    if (success) {
+      alert(message);
     }
   };
 
@@ -99,36 +97,10 @@ const NewEmployee = () => {
           onSubmit={handleSubmit}
           className="w-[70rem] h-[37rem] bg-white p-2 rounded-lg shadow-md mt-2 overflow-x-hidden overflow-scroll"
         >
-          <h2 className="text-2xl flex justify-center font-bold">Employee Information</h2>
+          <h2 className="text-2xl flex justify-center font-bold">
+            Employee Information
+          </h2>
 
-<<<<<<< HEAD
-          <div className="flex flex-col justify-evenly ">
-
-
-          <div className="w-full bg-slate-800 text-white text-lg p-3 font-semibold rounded-lg ">
-            Personal Information
-          </div>
-          {/* div 1 */}
-          <div className="w-[80%] mx-auto">
-          {/* employees Id  */}
-          <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2 "
-              htmlFor="employeeId"
-            >
-              ID
-            </label>
-            <input
-              type="text"
-              id="employeeId"
-              name="employeeId"
-              value={formData.employeeId}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-          </div>
-=======
           <div className="grid grid-cols-2 gap-4">
             {/* employees Id  */}
             <div className="mb-4">
@@ -187,18 +159,13 @@ const NewEmployee = () => {
               />
             </div>
 
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
             {/* EmployeeEmail */}
             <div className="mb-4">
               <label
                 className=" text-gray-700 text-sm font-bold mb-2"
                 htmlFor="employeeEmail"
               >
-<<<<<<< HEAD
-              Email
-=======
                 Email
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
               </label>
               <input
                 type="email"
@@ -217,15 +184,6 @@ const NewEmployee = () => {
                 className=" text-gray-700 text-sm font-bold mb-2"
                 htmlFor="gender"
               >
-<<<<<<< HEAD
-              Gender
-              </label>
-              <input
-                type="text"
-                id="gender"
-                name="gender"
-                value={formData.gender}
-=======
                 Password
               </label>
               <input
@@ -233,60 +191,55 @@ const NewEmployee = () => {
                 id="password"
                 name="password"
                 value={formData.password}
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
                 onChange={handleChange}
                 className="border border-black rounded-lg p-2 w-full"
                 required
               />
             </div>
-              {/* employee name  */}
-              <div className="mb-4">
-                <label
-                  className=" text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="employeeName"
-                >
-                    Name
-                </label>
-                <input
-                  type="text"
-                  id="employeeName"
-                  name="employeeName"
-                  value={formData.employeeName}
-                  onChange={handleChange}
-                  className="border border-black rounded-lg p-2 w-full"
-                  required
-                />
-              </div>
-                 {/* password */}
-                <div className="mb-4">
-                <label
-                  className=" text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="password"
-                >
+            {/* employee name  */}
+            <div className="mb-4">
+              <label
+                className=" text-gray-700 text-sm font-bold mb-2"
+                htmlFor="employeeName"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="employeeName"
+                name="employeeName"
+                value={formData.employeeName}
+                onChange={handleChange}
+                className="border border-black rounded-lg p-2 w-full"
+                required
+              />
+            </div>
+            {/* password */}
+            <div className="mb-4">
+              <label
+                className=" text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
                 Password
-                </label>
-                <input
-                  type="password "
-                  id="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  className="border border-black rounded-lg p-2 w-full"
-                  required
-                />
-              </div>
+              </label>
+              <input
+                type="password "
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="border border-black rounded-lg p-2 w-full"
+                required
+              />
+            </div>
 
-               {/* employeesPhoneNumber  */}
+            {/* employeesPhoneNumber  */}
             <div className="mb-4">
               <label
                 className=" text-gray-700 text-sm font-bold mb-2"
                 htmlFor="employeePhoneNumber"
               >
-<<<<<<< HEAD
-              Phone Number
-=======
                 Phone Number
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
               </label>
               <input
                 type="text"
@@ -299,54 +252,6 @@ const NewEmployee = () => {
               />
             </div>
 
-<<<<<<< HEAD
-                {/* Date of Birth */}
-              <div className="mb-4">
-                  <label
-                    className=" text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="dateOfBirth"
-                  >
-                  Date of Birth
-                  </label>
-                  <input
-                    type="date"
-                    id="dateOfBirth"
-                    name="dateOfBirth"
-                    value={formData.dateOfBirth}
-                    onChange={handleChange}
-                    className="border border-black rounded-lg p-2 w-full"
-                    required
-                  />
-              </div>
-
-              
-
-         
-          </div>
-          <div className="w-full bg-slate-800 text-white text-lg p-3 font-semibold rounded-lg ">
-            Personal Information
-          </div>
-          {/* div 2 */}
-
-          <div className="w-[80%] mx-auto" >
-             {/* Post office */}
-          <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="postOffice"
-            >
-            Post Office 
-            </label>
-            <input
-              type="text"
-              id="postOffice"
-              name="postOffice"
-              value={formData.postOffice}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-=======
             {/* Date of Birth */}
             <div className="mb-4">
               <label
@@ -441,7 +346,6 @@ const NewEmployee = () => {
                 className="border p-2 w-full"
                 required
               />
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
             </div>
 
             {/* state */}
@@ -463,47 +367,6 @@ const NewEmployee = () => {
               />
             </div>
 
-<<<<<<< HEAD
-          {/* Address */}
-          <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="address"
-            >
-            Address 
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-          </div>
-
-          {/* pinNumber */}
-          <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="pinNumber"
-            >
-              Pin Code 
-            </label>
-            <input
-              type="number"
-              id="pinNumber"
-              name="pinNumber"
-              value={formData.pinNumber}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-          </div>
-          
-
-=======
             {/* pinNumber */}
             <div className="mb-4">
               <label
@@ -560,109 +423,9 @@ const NewEmployee = () => {
                 required
               />
             </div>
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
 
             {/* city */}
             <div className="mb-4">
-<<<<<<< HEAD
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="city"
-            >
-            City 
-            </label>
-            <input
-              type="text"
-              id="city"
-              name="city"
-              value={formData.city}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-          </div>
-          {/* policeStation */}
-          <div className="mb-4">
-          <label
-            className=" text-gray-700 text-sm font-bold mb-2"
-            htmlFor="policeStation"
-          >
-          Police Station
-          </label>
-          <input
-            type="text"
-            id="policeStation"
-            name="policeStation"
-            value={formData.policeStation}
-            onChange={handleChange}
-            className="border border-black rounded-lg p-2 w-full"
-            required
-          />
-          </div>
-
-
-          </div>
-          <div className="w-full bg-slate-800 text-white text-lg p-3 font-semibold rounded-lg ">
-            Departmental Information
-          </div>
-
-
-          {/* div 3 */}
-          <div className="w-[80%] mx-auto">
-             {/* department */}
-             <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="department"
-              >
-              Department
-            </label>
-            <input
-              type="text"
-              id="department"
-              name="department"
-              value={formData.department}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-            </div>
-            {/* designation */}
-            <div className="mb-4">
-                <label
-                  className=" text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="designation"
-                >
-                  Designation
-                </label>
-                <input
-                  type="text"
-                  id="designation"
-                  name="designation"
-                  value={formData.designation}
-                  onChange={handleChange}
-                  className="border border-black rounded-lg p-2 w-full"
-                  required
-                />
-            </div>
-          {/* designationType */}
-          <div className="mb-4">
-            <label
-              className=" text-gray-700 text-sm font-bold mb-2"
-              htmlFor="designationType"
-            >
-              Designation Type 
-            </label>
-            <input
-              type="text"
-              id="designationType"
-              name="designationType"
-              value={formData.designationType}
-              onChange={handleChange}
-              className="border border-black rounded-lg p-2 w-full"
-              required
-            />
-=======
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="department"
@@ -681,22 +444,7 @@ const NewEmployee = () => {
             </div>
 
             {/* Add more fields as needed */}
->>>>>>> 81eb37b3a9745f577a0b5cd6595280461057e3c2
           </div>
-
-
-
-
-          </div>
-
-        {/* Add more fields as needed */}
-
-
-          </div>
-
-
-        
-
 
           <div className=" flex justify-center">
             <button
